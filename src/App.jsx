eb2fbb78d7fc1198fb3218443a7079239366c440
Sam Sinz <sam@sinz.fr>
2022-11-22 15:57:16 +0100

    lab done

---
+++ src/App.jsx
@@ -0,0 +1,83 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Contacts from "./contacts.json";
+import { useLayoutEffect, useState } from "react";
+
+function App() {
+  const [contacts, setContacts] = useState(Contacts.slice(4,9));
+
+  function getRandomContact() {
+    const remainingContacts = [...Contacts].filter(x => !contacts.includes(x))
+    const randomNb = Math.random() * remainingContacts.length;
+    const [randomContact] = remainingContacts.slice(randomNb, randomNb+1)
+    const copy = [...contacts]
+    copy.push(randomContact)
+    setContacts(copy)
+  }
+
+  function sortPopularity(){
+    const copy = [...contacts]
+    copy.sort((a, b) => a.popularity - b.popularity)
+    setContacts(copy)
+  }
+
+  function sortName(){
+    const copy = [...contacts]
+    copy.sort((a,b) => a.name.localeCompare(b.name))
+    setContacts(copy)
+  }
+
+  function handleDelete(idToDelete){
+    setContacts([...contacts].filter(x => x.id !== idToDelete))
+  }
+
+  return (
+    <div className="App">
+      <h1>IronContacts</h1>
+
+      <button onClick={getRandomContact}>Add Random Contact</button>
+      <button onClick={sortPopularity}>Sort by popularity</button>
+      <button onClick={sortName}>Sorty by name</button>
+
+      <table>
+        <tbody>
+          <tr>
+            <th>Picture</th>
+            <th>Name</th>
+            <th>Popularity</th>
+            <th>Won Oscar</th>
+            <th>Won Emmy</th>
+            <th>Actions</th>
+
+          </tr>
+          {contacts.map((element) => {
+            return (
+              <tr key={element.id}>
+                <td>
+                  <img src={element.pictureUrl} alt="" />
+                </td>
+                <td>
+                  <p>{element.name}</p>
+                </td>
+                <td>
+                  <p>{element.popularity.toFixed(2)}</p>
+                </td>
+                <td>
+                  <p>{element.wonOscar && '🏆'}</p>
+                </td>
+                <td>
+                  <p>{element.wonEmmy && '🏆'}</p>
+                </td>
+                <td>
+                  <button onClick={() => handleDelete(element.id)}>Delete</button>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default App;
